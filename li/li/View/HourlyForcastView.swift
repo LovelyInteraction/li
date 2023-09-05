f7b290ffa2f3e497d3f478b82f3203be9d5e7d77
eemdeeks <prince9088@gmail.com>
2023-09-06 01:12:56 +0900

    [#26]chore: 시간 보여주기

--- li/li/View/HourlyForcastView.swift
+++ li/li/View/HourlyForcastView.swift
@@ -10,22 +10,69 @@ import WeatherKit
 
 struct HourlyForcastView: View {
     
+    @State private var scrollOffset: CGFloat = 0
+    @State private var num: Int = 0
+    @State var time: String = "00:00"
+    
     let hourWeatherList: [HourWeather]
     
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(hourWeatherList, id: \.date) { hourWeatherItem in
-                    Image(systemName: "\(hourWeatherItem.symbolName).fill")
-                        .padding()
+        VStack{
+            Text("\(time)")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.vertical,5)
+                .padding(.horizontal)
+                .background(Color.black)
+                .cornerRadius(30)
+            //63
+            ScrollView(.horizontal) {
+                HStack {
+                    Color.clear
+                        .overlay(
+                            GeometryReader { geometry in
+                                Color.clear
+                                    .preference(key: OffsetPreferenceKey.self, value: geometry.frame(in: .named("scrollview")).origin.x)
+                            }
+                            .onPreferenceChange(OffsetPreferenceKey.self) { offset in
+                                self.scrollOffset = offset
+                                print("Offset: \(scrollOffset)")
+                                self.num = Int((scrollOffset - 16)/60)
+                                print("Offset: \(num)")
+                            }
+                        )
+                    ForEach(hourWeatherList, id: \.date) { hourWeatherItem in
+                        Image(systemName: "\(hourWeatherItem.symbolName).fill")
+                            .padding(15)
+                    }
+                }.padding()
+            }
+            .scrollIndicators(.hidden)
+            //wwdc23 발표 내용
+            //.scrollTargetBehavior(.paging)
+            .coordinateSpace(name: "scrollview")
+            .task(id: num) {
+                let calendar = Calendar.current
+                let components = calendar.dateComponents([.hour, .minute, .second], from: hourWeatherList[0].date)
+                if let now = components.hour {
+                    time = String(now - num) + ":00"
                 }
-            }.padding()
-        }.scrollIndicators(.hidden)
+            }
+        }
+        
     }
 }
 
 struct HourlyForcastView_Previews: PreviewProvider {
     static var previews: some View {
         HourlyForcastView(hourWeatherList: [])
+    }
+}
+
+struct OffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
     }
 }
