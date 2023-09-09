e2d51112eb07610915820114ee2aa7f41c501c7c
eemdeeks <prince9088@gmail.com>
2023-09-09 16:13:02 +0900

    [#34]chore: 스크롤뷰 드래그 시 햅틱 추가

---
+++ li/li/Manager/HapticManager.swift
@@ -0,0 +1,24 @@
+//
+//  HapticManager.swift
+//  li
+//
+//  Created by 박승찬 on 2023/09/09.
+//
+
+import Foundation
+import UIKit
+
+class HapticManager {
+    
+    static let instance = HapticManager()
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
